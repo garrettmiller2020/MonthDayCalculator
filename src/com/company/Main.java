a7a2135941cdb0ed6747944b4c36526b7976db25
Garrett <garrett.miller.2020@student.lhschools.org>
2020-04-30 12:29:12 -0400

    Printing Poem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Monday's child is fair of face,");
+        System.out.println("Tuesday's child is full of grace,");
+        System.out.println("Wednesday's child is full of woe,");
+        System.out.println("Thursday's child has far to go,");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("Saturday's child works hard for a living,");
+        System.out.println("But the child born on the Sabbath Day,");
+        System.out.println("Is fair and wise and good in every way,");
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter month(1-12): ");
